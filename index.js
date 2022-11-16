8cfc2491941bdabcb2ff5f674300462a8c63bb85
tugbacanbaz <tugbacanbaz.pi@gmail.com>
2022-11-16 23:53:36 +0300

    Complited Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const gifts = ['Guadalupe', 'Ollie', 'Aki'];
+
+const writeCards=(gifts)=> {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Thank you, ${gifts[i]}, for the wonderful suprise gift!`);
+  }
+
+  return gifts;
+}
+/* writeCards(gifts); */
+
+const countDown=(integer)=>{
+    let i=integer
+    while(i>-1){
+        console.log(i)
+        i--
+    }
+}
+/* countDown(10); */
